c21a3c521a4ccbfd7749867e1be071afe9e29456
drichard1989 <drichard1989@icloud.com>
2017-02-01 08:31:05 -0600

    Added interactivity between the inquirer function and the data, but I keep getting an error saying that item_id = 1 is undefined.

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,5 +1,7 @@
 const mysql = require("mysql");
 const inquirer = require("inquirer");
+const Table = require ("cli-table");
+
 
 var connection = mysql.createConnection(
 {
@@ -36,25 +38,44 @@ inquirer.prompt([
 		type: "input", 
 		message: "How many units of the product would you like to buy?",
 		name: "amount"
-	},
-
-	{
-		type: "confirm",
-		message: "Are you sure?",
-		name: "confirm",
-		default: true
 	}
 
-]).then(function(user){
 
-	console.log(JSON.stringify(user, null, 2));
 
-	if (user.confirm){
-		console.log("You want " + user.id);
-		console.log("You want " + user.amount + " of them.");
-	}
+]).then(function(response){
 
-	else{
-		console.log("Come back when you are ready.")
-	}
+	// Now, we are going to select the stock_quantitycolumn from the products table where the item_id is equal to the id entered by the user.
+	connection.query("SELECT stock_quantity, product_name, price FROM products WHERE item_id = " + response.id, function(error, res){
+
+		if (error){
+			throw error;
+		}
+
+		// Now, we are going to write a conditional if statement that says if there is more in stock than requested, then we are going to update the amount available using the amount purchased as the reduction amount.
+
+		if (res[0].stock_quantity >= response.amount) {
+			var updateStockQuantity = res[0].stock_quantity - response.amount;
+			connection.query('UPDATE products SET stock_quantity = ' + updateStockQuantity + "WHERE item_id = " + response.chosen_id, function(error, result){
+				if(error){
+					throw error;
+				}
+			});
+			
+			var yourCost = res[0].price * response.amount;
+			console.log("Amount you spent: " + yourCost);
+
+		}
+
+		else if(res.stock_quantity === 0){
+			console.log("We are sorry, we dont have " + res[0].product_name + "at this time.");
+
+		}
+
+		else {
+			console.log("We do not have enough of " + res[0].product_name + " in stock to sell you.")
+		}
+
+	});
 });
+
+
